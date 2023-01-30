5c751c839963e27f93790ed364a58b7fa01c5ab3
ilentiy <i.brykov9@bk.ru>
2023-01-31 01:34:34 +0300

    initial commit

---
+++ Tesla/Tesla/TeslaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeslaApp.swift
+//  Tesla
+//
+//  Created by Илья on 31.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct TeslaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
